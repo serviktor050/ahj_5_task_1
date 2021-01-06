1489f0fd3fba85255b6091acaa950663f76ea3c6
serviktor050 <mr.s-gagulin@yandex.ru>
2021-01-06 18:50:45 +0300

    Настройка сборки

--- webpack.common.js
+++ webpack.common.js
@@ -3,11 +3,10 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    port: 9000,
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -33,38 +32,8 @@ module.exports = {
         ],
       },
       {
-        test: /\.txt$/i,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
+        test: /\.svg$/,
+        type: 'asset/resource',
       },
     ],
   },
